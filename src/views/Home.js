70b219c0320547343cf38ac341019ae53cc3022b
harsh-nexuslink <harsh.devisha@nexuslinkservices.in>
2022-04-12 19:57:49 +0530

    init homepage/dashboard

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,32 @@
 import { useState, useEffect } from "react"
+import { Row, Col } from "reactstrap"
+import axios from "@axios"
 // ** Utils
 
 // ** Store & Actions
-import { Card, CardHeader, CardTitle } from "reactstrap"
+import { Card, CardHeader, CardBody, CardTitle } from "reactstrap"
 
 const Home = () => {
+  const fetchChartData = () => {
+    const body = {
+      order_user: "innoa"
+    }
+    axios.post("/OrderTotalQuery", body).then((res) => console.log(res))
+  }
+
+  useEffect(() => {
+    fetchChartData()
+  }, [])
+
   return (
     <div>
       <Card>
-        <CardHeader></CardHeader>
+        <CardHeader>
+          <h4>Dashboard</h4>
+        </CardHeader>
+        <CardBody>
+          <Row></Row>
+        </CardBody>
       </Card>
     </div>
   )
