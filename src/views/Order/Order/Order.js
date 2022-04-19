5cd5c20ee0adb044f25ad048fd4f0a69e62896c9
harsh-nexuslink <harsh.devisha@nexuslinkservices.in>
2022-04-19 13:33:50 +0530

    refactored to make Stepper dynamic and seperate component

--- src/views/Order/Order/Order.js
+++ src/views/Order/Order/Order.js
@@ -1,103 +1,26 @@
-import { useState, useEffect } from "react"
-import { Row, Col } from "reactstrap"
-import { Check, FileMinus } from "react-feather"
-import axios from "@axios"
-import Chart from "react-apexcharts"
-import DataTable from "react-data-table-component"
-import CheckBox from "@components/CheckBox/CheckBox"
-import Select from "react-select"
+import { useState } from "react"
 import SelectItem from "./SelectItem"
 import OrderForm from "./OrderForm"
 import PreviewAndSummary from "./PreviewAndSummary"
 import InvoiceAndDelivery from "./InvoiceAndDeliveryDate"
 import Payment from "./Payment"
-import Stepper from "./Stepper"
+import Stepper from "../../Stepper"
 import DirectPrint from "./DirectPrint"
 import { Link } from "react-router-dom"
-import {
-  Card,
-  CardHeader,
-  Spinner,
-  CardBody,
-  CardTitle,
-  CardFooter,
-  Breadcrumb,
-  BreadcrumbItem,
-  Label,
-  Input,
-  Button
-} from "reactstrap"
-import { ArrowRight, ArrowLeft } from "react-feather"
+import { Breadcrumb, BreadcrumbItem } from "reactstrap"
+
+const stepperMenu = [
+  "Select Item",
+  "Order Form",
+  "Preview Item & Summary Size Table",
+  "Invoice & Delivery Date",
+  "Payment",
+  "Direct Print"
+]
 
 const Order = () => {
   // APP states
-  const [brandOptions, setBrandOptions] = useState([])
-  const [itemTypeOptions, setItemTypeOptions] = useState([])
-  const [itemList, setItemList] = useState([])
-  const [visibleCardIndex, setVisibleCardIndex] = useState(0)
-  const [checkedList, setCheckedList] = useState([])
-  const [loader, setLoader] = useState(false)
-  const [currentStep, setCurrentStep] = useState(1)
-
-  const handleCheckListChange = (id) => {
-    let tempList = checkedList
-    if (checkedList.includes(id)) {
-      tempList.splice(tempList.indexOf(id), 1)
-      setCheckedList([...tempList])
-    } else {
-      setCheckedList([...tempList, id])
-    }
-  }
-
-  // API services
-  const fetchBrandList = () => {
-    const body = {
-      order_user: "innoa"
-    }
-    axios.post("/Brand/GetBrandListByClient", body).then((res) => {
-      if (res.status === 200) {
-        setBrandOptions(
-          res.data.map((br) => ({ value: br.guid_key, label: br.brand_name }))
-        )
-      }
-    })
-  }
-
-  const fetchItemTypeOptions = () => {
-    axios.post("/Item/GetItemTypeList").then((res) => {
-      if (res.status === 200) {
-        setItemTypeOptions(
-          res.data.map((itm) => ({
-            value: itm.id,
-            label: itm.item_type_name,
-            parent_id: itm.parent_id
-          }))
-        )
-      }
-    })
-  }
-
-  const fetchItemList = (brand = "", item_type = "") => {
-    setLoader(true)
-    setVisibleCardIndex(0)
-    const body = {
-      order_user: "innoa",
-      brand_key: brand?.value,
-      item_ref_type: item_type?.value
-    }
-    axios.post("/Item/GetItemRefList", body).then((res) => {
-      if (res.status === 200) {
-        setLoader(false)
-        setItemList(res.data)
-      }
-    })
-  }
-
-  useEffect(() => {
-    fetchBrandList()
-    fetchItemTypeOptions()
-    fetchItemList()
-  }, [])
+  const [currentStep, setCurrentStep] = useState(0)
 
   return (
     <div>
@@ -119,24 +42,28 @@ const Order = () => {
           </BreadcrumbItem>
         </Breadcrumb>
       </div>
-      <Stepper currentStep={currentStep} setCurrentStep={setCurrentStep}/>
-      {currentStep === 1 ? (
+      <Stepper
+        stepperMenu={stepperMenu}
+        currentStep={currentStep}
+        setCurrentStep={setCurrentStep}
+      />
+      {currentStep === 0 ? (
         <SelectItem setCurrentStep={setCurrentStep} currentStep={currentStep} />
-      ) : currentStep === 2 ? (
+      ) : currentStep === 1 ? (
         <OrderForm setCurrentStep={setCurrentStep} currentStep={currentStep} />
-      ) : currentStep === 3 ? (
+      ) : currentStep === 2 ? (
         <PreviewAndSummary
           setCurrentStep={setCurrentStep}
           currentStep={currentStep}
         />
-      ) : currentStep === 4 ? (
+      ) : currentStep === 3 ? (
         <InvoiceAndDelivery
           setCurrentStep={setCurrentStep}
           currentStep={currentStep}
         />
-      ) : currentStep === 5 ? (
+      ) : currentStep === 4 ? (
         <Payment setCurrentStep={setCurrentStep} currentStep={currentStep} />
-      ) : currentStep === 6 ? (
+      ) : currentStep === 5 ? (
         <DirectPrint
           setCurrentStep={setCurrentStep}
           currentStep={currentStep}
---
+++ src/views/Stepper.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import { Check, FileMinus } from "react-feather"
+import { Breadcrumb, BreadcrumbItem } from "reactstrap"
+
+const Stepper = (props) => {
+  return (
+    <div style={{ paddingBottom: "10px" }}>
+      <Breadcrumb>
+        {props.stepperMenu.map((item, index) => (
+          <BreadcrumbItem>
+            <div
+              className="custom-stepper"
+              onClick={() => props.setCurrentStep(index)}
+            >
+              <div
+                id="selectItemIcon"
+                className={
+                  props.currentStep === index
+                    ? "stepper-active-icon"
+                    : props.currentStep > index
+                    ? "stepper-passed-icon"
+                    : "stepper-pending-icon"
+                }
+              >
+                <FileMinus size={25} />
+              </div>
+              <div>
+                <h5
+                  className={
+                    props.currentStep === index
+                      ? "stepper-active-text"
+                      : props.currentStep > index
+                      ? "stepper-passed-text"
+                      : "stepper-pending-text"
+                  }
+                >
+                  {item}
+                </h5>
+              </div>
+            </div>
+          </BreadcrumbItem>
+        ))}
+      </Breadcrumb>
+    </div>
+  )
+}
+
+export default Stepper
