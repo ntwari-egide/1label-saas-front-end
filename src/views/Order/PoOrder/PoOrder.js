9675d804e90b4ff260e76f2195bf0628e37419b6
harsh-nexuslink <harsh.devisha@nexuslinkservices.in>
2022-05-05 18:48:57 +0530

    refactored to implement multiple component inside module.

--- src/views/Order/PoOrder/PoOrder.js
+++ src/views/Order/PoOrder/PoOrder.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 import Listing from "./Listing/listing"
 import { useTranslation } from "react-i18next"
+import OrderForm from "./OrderForm"
 
 const stepperMenu = [
   "Listing",
@@ -21,6 +22,13 @@ const PoOrder = () => {
   const { t } = useTranslation()
   // app states
   const [currentStep, setCurrentStep] = useState(0)
+  const [lastStep, setLastStep] = useState(stepperMenu.length - 1)
+  // Order Form data
+  const [dynamicFieldData, setDynamicFieldData] = useState([])
+  const [fibreInstructionData, setFibreInstructionData] = useState([{}])
+  const [defaultContentData, setDefaultContentData] = useState([{}])
+  const [careData, setCareData] = useState([{}])
+
   return (
     <div>
       <div style={{ display: "flex" }}>
@@ -46,7 +54,27 @@ const PoOrder = () => {
         currentStep={currentStep}
         setCurrentStep={setCurrentStep}
       />
-      {currentStep === 0 ? <Listing /> : null}
+      {currentStep === 0 ? (
+        <Listing
+          currentStep={currentStep}
+          setCurrentStep={setCurrentStep}
+          lastStep={lastStep}
+        />
+      ) : currentStep === 2 ? (
+        <OrderForm
+          currentStep={currentStep}
+          setCurrentStep={setCurrentStep}
+          lastStep={lastStep}
+          dynamicFieldData={dynamicFieldData}
+          setDynamicFieldData={setDynamicFieldData}
+          fibreInstructionData={fibreInstructionData}
+          setFibreInstructionData={setFibreInstructionData}
+          defaultContentData={defaultContentData}
+          setDefaultContentData={setDefaultContentData}
+          careData={careData}
+          setCareData={setCareData}
+        />
+      ) : null}
     </div>
   )
 }
