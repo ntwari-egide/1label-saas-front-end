a05c86a514aaad7db71e61cbfa964caf064a7e75
harsh-nexuslink <harsh.devisha@nexuslinkservices.in>
2022-05-06 19:53:29 +0530

    layout and integrated APIs

--- src/views/Order/PoOrder/SizeTable/SizeTable.js
+++ src/views/Order/PoOrder/SizeTable/SizeTable.js
@@ -1,13 +1,252 @@
-import { Card, CardHeader, CardBody, CardFooter } from "reactstrap"
+import { useState, useEffect } from "react"
+import {
+  Row,
+  Spinner,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter
+} from "reactstrap"
 import Footer from "../../../CommonFooter"
+import axios from "@axios"
+import Select from "react-select"
+import DataTable from "react-data-table-component"
+import { XMLParser } from "fast-xml-parser"
+import { useTranslation } from "react-i18next"
 
 const SizeTable = (props) => {
+  const { t } = useTranslation()
+  const [sizeContentData, setSizeContentData] = useState([])
+  const [wastageStatus, setWastageStatus] = useState(true)
+  const [wastageOptions, setWastageOptions] = useState([])
+  const [loader, setLoader] = useState(false)
+
+  const sizeCols = [
+    {
+      name: t("SIZE DESCRIPTION"),
+      selector: "Size Description",
+      sortable: true
+    },
+    {
+      name: t("SIZE"),
+      selector: "Size",
+      sortable: true
+    },
+    {
+      name: t("SUPPLIER REF"),
+      selector: "Supplier Ref",
+      sortable: false
+    },
+    {
+      name: t("SUPPLIER COLOR"),
+      selector: "Supplier Colour",
+      sortable: false
+    },
+    {
+      name: t("OPTION ID"),
+      selector: "Option ID",
+      sortable: false
+    },
+    {
+      name: t("BUYING GROUP ID"),
+      selector: "Buying Group ID",
+      sortable: false
+    },
+    {
+      name: t("PRODUCT GROUP DESCRIPTION"),
+      selector: "Product Group Description",
+      width: "200px",
+      sortable: false
+    },
+    {
+      name: t("SKU Code"),
+      selector: "SKU Code",
+      sortable: false
+    },
+    {
+      name: t("STYLE NUMBER"),
+      selector: "Style Number",
+      sortable: false
+    },
+    {
+      name: t("BARCODE"),
+      selector: "Barcode",
+      sortable: false
+    },
+    {
+      name: t("QTY ITEM REF 1"),
+      selector: "QTY ITEM REF 1"
+    },
+    {
+      name: t("QTY ITEM REF 2"),
+      selector: "QTY ITEM REF 2"
+    },
+    {
+      name: t("QTY ITEM REF 3"),
+      selector: "QTY ITEM REF 3"
+    },
+    {
+      name: t("QTY ITEM REF 4"),
+      selector: "QTY ITEM REF 4"
+    },
+    {
+      name: t("QTY ITEM REF 5"),
+      selector: "QTY ITEM REF 5"
+    },
+    {
+      name: t("QTY ITEM REF 6"),
+      selector: "QTY ITEM REF 6"
+    },
+    {
+      name: t("QTY ITEM REF 7"),
+      selector: "QTY ITEM REF 7"
+    },
+    {
+      name: t("QTY ITEM REF 8"),
+      selector: "QTY ITEM REF 8"
+    },
+    {
+      name: t("QTY ITEM REF 9"),
+      selector: "QTY ITEM REF 9"
+    },
+    {
+      name: t("QTY ITEM REF 10"),
+      selector: "QTY ITEM REF 10"
+    },
+    {
+      name: t("UPC/EAN CODE"),
+      selector: "UPC/EAN CODE"
+    }
+  ]
+
+  const formatColToRow = (xmlStr) => {
+    const parser = new XMLParser()
+    const jsObj = parser.parse(xmlStr)
+    const nRows = Object.keys(jsObj?.SizeMatrix?.Table).length - 2 // gets the no of rows
+    let data = [] // initialized data to fill row by row
+    let currentRow = 0 + 2 // because actual data begins at Column2
+    for (let i = 0; i < nRows; i++) {
+      let row = {} // initialise empty row
+      jsObj?.SizeMatrix?.Table?.map((col) => {
+        row[col["Column1"]] = col[`Column${currentRow}`] // row[column_name] = column_value
+      })
+      data.push(row) // push the row to data
+      currentRow += 1 // increment row count
+    }
+    return data
+  }
+
+  // API Services
+  const fetchSizeTable = () => {
+    const body = {
+      brand_key: props.brand ? props.brand.value : "",
+      order_key: props.combinedPOOrderKey || ""
+    }
+    const tempState = []
+    axios
+      .post("/order/GetPOSizeTableTempList", body)
+      .then((res) => {
+        setLoader(true)
+        if (res.status === 200) {
+          res.data.map((dt, index) => {
+            if (dt.size_content) {
+              tempState[index] = formatColToRow(dt.size_content)
+            }
+          })
+        }
+        setSizeContentData([...tempState])
+        setLoader(false)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  const fetchWastageList = () => {
+    const body = {
+      brand_key: props.brand ? props.brand.value : ""
+    }
+    axios
+      .post("/Brand/GetWastageList", body)
+      .then((res) => {
+        if (res.status === 200) {
+          if (res.data[0]?.show_status === "Y") {
+            setWastageStatus(true)
+          } else {
+            setWastageStatus(false)
+          }
+          setWastageOptions(res.data[0]?.wastage_value)
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchSizeTable()
+    fetchWastageList()
+  }, [])
+
   return (
     <Card>
       <CardHeader>
         <h4>Size Table</h4>
       </CardHeader>
-      <CardBody></CardBody>
+      <CardBody>
+        {loader ? (
+          <div
+            style={{
+              display: "flex",
+              height: "500px",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <div>
+              <Spinner color="primary" />
+            </div>
+          </div>
+        ) : (
+          <div>
+            <Row style={{ margin: 0, marginBottom: "10px" }}>
+              <DataTable
+                data={sizeContentData[0]}
+                columns={sizeCols}
+                noHeader={true}
+              />
+            </Row>
+            {sizeContentData[1] ? (
+              <Row style={{ margin: 0, marginBottom: "10px" }}>
+                <DataTable
+                  data={sizeContentData[1]}
+                  columns={sizeCols}
+                  noHeader={true}
+                />
+              </Row>
+            ) : null}
+          </div>
+        )}
+        <Row>
+          <Col xs="12" sm="12" md="3" lg="1" xl="1">
+            <div
+              style={{
+                display: "flex",
+                height: "100%",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <div>Wastage:</div>
+            </div>
+          </Col>
+          <Col xs="12" sm="12" md="3" lg="2" xl="2">
+            <Select
+              className="React"
+              classNamePrefix="select"
+              options={wastageOptions}
+            />
+          </Col>
+        </Row>
+      </CardBody>
       <CardFooter>
         <Footer
           currentStep={props.currentStep}
