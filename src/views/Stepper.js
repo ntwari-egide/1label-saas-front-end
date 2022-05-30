dd2e7ae847898532c263175854a2b73730aff87a
harsh-nexuslink <harsh.devisha@nexuslinkservices.in>
2022-05-30 20:08:37 +0530

    refactor: refactored entire validation scheme to tidy up component.

--- src/views/Stepper.js
+++ src/views/Stepper.js
@@ -1,57 +1,76 @@
 import { useState, useEffect } from "react"
-import { Check, FileMinus } from "react-feather"
+import { FileMinus } from "react-feather"
 import { Breadcrumb, BreadcrumbItem } from "reactstrap"
 import { useTranslation } from "react-i18next"
 
 const Stepper = (props) => {
   const { t } = useTranslation()
+
+  const normalSelectedItemValidation = () => {
+    if (
+      props.validationFields?.selectedItems &&
+      props.validationFields?.selectedItems?.length <= 0
+    ) {
+      alert("Please select an item to continue")
+      return false
+    }
+    return true
+  }
+
+  const poSelectedItemsValidation = () => {
+    if (
+      props.component === "POOrder" &&
+      props.validationFields?.poSelectedItems &&
+      props.validationFields?.poSelectedItems?.length <= 0
+    ) {
+      alert("Please select an item to continue")
+      return false
+    }
+    return true
+  }
+
+  const normalOrderFormManFieldsValidation = (menuItem) => {
+    // to end the loop when one of the man fields is empty
+    let localFlag = true
+    if (props.validationFields?.orderFormManFields) {
+      Object.keys(props.validationFields.orderFormManFields).map((field) => {
+        if (
+          props.validationFields.orderFormManFields[field].length <= 0 &&
+          props.currentStep === 1 &&
+          localFlag &&
+          menuItem != "Select Item"
+        ) {
+          const tempState = props.orderFormManFields
+          tempState[field] = true
+          props.setOrderFormManFields({ ...tempState })
+          alert("Please enter mandatory fields")
+          localFlag = false
+        }
+      })
+    }
+    return localFlag
+  }
+
   return (
     <div style={{ paddingBottom: "10px" }}>
       <Breadcrumb>
-        {props.stepperMenu.map((item, index) => (
+        {props.stepperMenu.map((menuItem, index) => (
           <BreadcrumbItem>
             <div
               className="custom-stepper"
               onClick={() => {
-                let validationPassed = true
+                // let validationPassed = true
                 // validation for selected items
-                if (
-                  props.validationFields?.selectedItems &&
-                  props.validationFields?.selectedItems?.length <= 0
-                ) {
-                  alert("Please select an item to continue")
-                  validationPassed = false
-                }
+                // validationPassed = normalSelectedItemValidation()
                 // validation for poselectedtiems
-                if (
-                  props.validationFields?.poSelectedItems &&
-                  props.validationFields?.poSelectedItems?.length <= 0
-                ) {
-                  alert("Please select an item to continue")
-                  validationPassed = false
-                }
-
+                // validationPassed = poSelectedItemsValidation()
                 // validation for Order Form mandatory fields
-                if (props.validationFields?.orderFormManFields) {
-                  Object.keys(props.validationFields.orderFormManFields).map(
-                    (field) => {
-                      if (
-                        props.validationFields.orderFormManFields[field]
-                          .length <= 0 &&
-                        props.currentStep === 1
-                      ) {
-                        const tempState = props.orderFormManFields
-                        tempState[field] = true
-                        props.setOrderFormManFields({ ...tempState })
-                        validationPassed = false
-                      }
-                    }
-                  )
-                  if (!validationPassed && props.currentStep === 1) {
-                    alert("Please enter mandatory fields")
-                  }
-                }
-                if (validationPassed) {
+                // validationPassed = normalOrderFormManFieldsValidation()
+                if (
+                  normalSelectedItemValidation() &&
+                  poSelectedItemsValidation() &&
+                  normalOrderFormManFieldsValidation(menuItem)
+                ) {
                   props.setCurrentStep(index)
                 }
               }}
@@ -78,7 +97,7 @@ const Stepper = (props) => {
                       : "stepper-pending-text"
                   }
                 >
-                  {t(`${item}`)}
+                  {t(`${menuItem}`)}
                 </h5>
               </div>
             </div>
