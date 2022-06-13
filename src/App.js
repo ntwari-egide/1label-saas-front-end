0ef473bd7fa5fab15545eabf991463aba5588c8d
harsh-nexuslink <harsh.devisha@nexuslinkservices.in>
2022-06-13 12:24:18 +0530

    refactor: formatted with prettier

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@
 
 // export default App
 // ** Router Import
-import Router from './router/Router'
-import BlockUi from 'react-block-ui'
-import { Spinner } from 'reactstrap'
-import 'react-block-ui/style.css'
-import { connect } from 'react-redux'
+import Router from "./router/Router"
+import BlockUi from "react-block-ui"
+import { Spinner } from "reactstrap"
+import "react-block-ui/style.css"
+import { connect } from "react-redux"
 // style={{ height: 'fit-content', minHeight: '100vh' }}
 
-const App = props => <BlockUi tag="body" blocking={props.loading} loader={<Spinner type='grow' size='lg' color="primary" />}><Router /></BlockUi>
+const App = (props) => (
+  <BlockUi
+    tag="body"
+    blocking={props.loading}
+    loader={<Spinner type="grow" size="lg" color="primary" />}
+  >
+    <Router />
+  </BlockUi>
+)
 
 const mapStateToProps = (state) => {
-    return {
-        loading: state.layout.sidebarLeftLoading
-    }
+  return {
+    loading: state.layout.sidebarLeftLoading
+  }
 }
 
 export default connect(mapStateToProps, null)(App)
