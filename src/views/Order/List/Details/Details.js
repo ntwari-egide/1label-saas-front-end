8b1255cbf11315867c7a33b5cdd709ad1cf084b0
harsh-nexuslink <harsh.devisha@nexuslinkservices.in>
2022-06-27 12:46:09 +0530

    debug: fixed footer position issue.

--- src/views/Order/List/Details/Details.js
+++ src/views/Order/List/Details/Details.js
@@ -133,150 +133,152 @@ const Details = (props) => {
           <Spinner color="primary" />
         </div>
       ) : (
-        <Row>
-          <Col xs="12" sm="12" md="12" lg="8" xl="8">
-            {itemList.map((item, index) => (
-              <Row>
-                <Col>
-                  <Card>
-                    <CardBody>
-                      <Row>
-                        <Col xs="12" sm="12" md="8" lg="8" xl="8">
-                          <Row>
-                            <Col>
-                              <h7
-                                style={{
-                                  fontWeight: "bolder",
-                                  marginBottom: "5px"
-                                }}
-                              >
-                                {item?.item_ref_desc}
-                              </h7>
-                              <p>
-                                By <Link>{item?.brand_name}</Link>
-                              </p>
-                            </Col>
-                          </Row>
-                          <Row style={{ maringTop: "10px" }}>
-                            <Col xs="3" sm="3" md="3" lg="3" xl="3">
-                              <Radio
-                                label="Delivery"
-                                name={`item${index}`}
-                                disabled={props.isOrderConfirmed}
-                              />
-                            </Col>
-                            <Col xs="3" sm="3" md="3" lg="3" xl="3">
-                              <Radio
-                                label="Direct Print"
-                                name={`item${index}`}
-                                disabled={props.isOrderConfirmed}
-                              />
-                            </Col>
-                          </Row>
-                        </Col>
-                        <Col
-                          xs="12"
-                          sm="12"
-                          md="4"
-                          lg="4"
-                          xl="4"
-                          style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center"
-                          }}
-                        >
-                          <Row style={{ width: "100%" }}>
-                            <Col>
-                              <Button
-                                color="primary"
-                                style={{ width: "100%" }}
-                                disabled={props.isOrderConfirmed}
-                                onClick={() => handleRemoveItem(index)}
-                              >
-                                <X size={17} />
-                                Remove
-                              </Button>
-                            </Col>
-                          </Row>
-                        </Col>
-                      </Row>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
-            ))}
-          </Col>
-          <Col xs="12" sm="12" md="12" lg="4" xl="4">
-            <Card>
-              <CardHeader>
-                <p className="text-muted">OPTIONS</p>
-              </CardHeader>
-              <CardBody>
-                <Row>
-                  <Col>Price Details</Col>
-                </Row>
-                <Row style={{ marginTop: "15px" }}>
-                  <Col xs="6" sm="6" md="6" lg="6" xl="6">
-                    <p className="text-muted" style={{ marginBottom: 0 }}>
-                      Total MRP
-                    </p>
-                  </Col>
-                  <Col xs="6" sm="6" md="6" lg="6" xl="6">
-                    <div style={{ textAlign: "right" }}>
-                      <p style={{ marginBottom: 0 }}>$0.1</p>
-                    </div>
-                  </Col>
-                </Row>
-                <Row style={{ marginTop: "10px" }}>
-                  <Col xs="6" sm="6" md="6" lg="6" xl="6">
-                    <p className="text-muted" style={{ marginBottom: 0 }}>
-                      Delivery Charges
-                    </p>
-                  </Col>
-                  <Col xs="6" sm="6" md="6" lg="6" xl="6">
-                    <div style={{ textAlign: "right" }}>
-                      <p style={{ marginBottom: 0 }}>$10</p>
-                    </div>
-                  </Col>
-                </Row>
-                <Row>
-                  <hr
-                    style={{
-                      widht: "100%",
-                      border: "1px solid",
-                      color: "#80808026",
-                      marginLeft: "10px",
-                      marginRight: "10px"
-                    }}
-                  />
-                </Row>
-                <Row style={{ marginTop: "15px" }}>
-                  <Col xs="6" sm="6" md="6" lg="6" xl="6">
-                    <p style={{ marginBottom: 0 }}>Total</p>
-                  </Col>
-                  <Col xs="6" sm="6" md="6" lg="6" xl="6">
-                    <div style={{ textAlign: "right" }}>
-                      <p style={{ marginBottom: 0 }}>$10.1</p>
-                    </div>
-                  </Col>
-                </Row>
+        <div style={{ minHeight: "calc(100vh - 245px)" }}>
+          <Row>
+            <Col xs="12" sm="12" md="12" lg="8" xl="8">
+              {itemList.map((item, index) => (
                 <Row>
                   <Col>
-                    <Button
-                      color="primary"
-                      style={{ width: "100%", marginTop: "5px" }}
-                      disabled={props.isOrderConfirmed}
-                      onClick={confirmOrder}
-                    >
-                      Place Order
-                    </Button>
+                    <Card>
+                      <CardBody>
+                        <Row>
+                          <Col xs="12" sm="12" md="8" lg="8" xl="8">
+                            <Row>
+                              <Col>
+                                <h7
+                                  style={{
+                                    fontWeight: "bolder",
+                                    marginBottom: "5px"
+                                  }}
+                                >
+                                  {item?.item_ref_desc}
+                                </h7>
+                                <p>
+                                  By <Link>{item?.brand_name}</Link>
+                                </p>
+                              </Col>
+                            </Row>
+                            <Row style={{ maringTop: "10px" }}>
+                              <Col xs="3" sm="3" md="3" lg="3" xl="3">
+                                <Radio
+                                  label="Delivery"
+                                  name={`item${index}`}
+                                  disabled={props.isOrderConfirmed}
+                                />
+                              </Col>
+                              <Col xs="3" sm="3" md="3" lg="3" xl="3">
+                                <Radio
+                                  label="Direct Print"
+                                  name={`item${index}`}
+                                  disabled={props.isOrderConfirmed}
+                                />
+                              </Col>
+                            </Row>
+                          </Col>
+                          <Col
+                            xs="12"
+                            sm="12"
+                            md="4"
+                            lg="4"
+                            xl="4"
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center"
+                            }}
+                          >
+                            <Row style={{ width: "100%" }}>
+                              <Col>
+                                <Button
+                                  color="primary"
+                                  style={{ width: "100%" }}
+                                  disabled={props.isOrderConfirmed}
+                                  onClick={() => handleRemoveItem(index)}
+                                >
+                                  <X size={17} />
+                                  Remove
+                                </Button>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
                   </Col>
                 </Row>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+              ))}
+            </Col>
+            <Col xs="12" sm="12" md="12" lg="4" xl="4">
+              <Card>
+                <CardHeader>
+                  <p className="text-muted">OPTIONS</p>
+                </CardHeader>
+                <CardBody>
+                  <Row>
+                    <Col>Price Details</Col>
+                  </Row>
+                  <Row style={{ marginTop: "15px" }}>
+                    <Col xs="6" sm="6" md="6" lg="6" xl="6">
+                      <p className="text-muted" style={{ marginBottom: 0 }}>
+                        Total MRP
+                      </p>
+                    </Col>
+                    <Col xs="6" sm="6" md="6" lg="6" xl="6">
+                      <div style={{ textAlign: "right" }}>
+                        <p style={{ marginBottom: 0 }}>$0.1</p>
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row style={{ marginTop: "10px" }}>
+                    <Col xs="6" sm="6" md="6" lg="6" xl="6">
+                      <p className="text-muted" style={{ marginBottom: 0 }}>
+                        Delivery Charges
+                      </p>
+                    </Col>
+                    <Col xs="6" sm="6" md="6" lg="6" xl="6">
+                      <div style={{ textAlign: "right" }}>
+                        <p style={{ marginBottom: 0 }}>$10</p>
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <hr
+                      style={{
+                        widht: "100%",
+                        border: "1px solid",
+                        color: "#80808026",
+                        marginLeft: "10px",
+                        marginRight: "10px"
+                      }}
+                    />
+                  </Row>
+                  <Row style={{ marginTop: "15px" }}>
+                    <Col xs="6" sm="6" md="6" lg="6" xl="6">
+                      <p style={{ marginBottom: 0 }}>Total</p>
+                    </Col>
+                    <Col xs="6" sm="6" md="6" lg="6" xl="6">
+                      <div style={{ textAlign: "right" }}>
+                        <p style={{ marginBottom: 0 }}>$10.1</p>
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Button
+                        color="primary"
+                        style={{ width: "100%", marginTop: "5px" }}
+                        disabled={props.isOrderConfirmed}
+                        onClick={confirmOrder}
+                      >
+                        Place Order
+                      </Button>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       )}
     </div>
   )
